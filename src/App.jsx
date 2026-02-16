eb176ecd613563fef9b3edf9e6f9e8043b823a08
jus <jusphean@gmail.com>
2026-02-15 19:29:31 -0800

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <HeroSection />
     </main>
   )
 }
