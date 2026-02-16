6165a0b77d2330d4fe6e9fc724d64b8964ed0041
jus <jusphean@gmail.com>
2026-02-16 11:52:39 -0800

    text-reveal-animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <div className='h-dvh border border-red-500'></div>
     </main>
   )
 }
